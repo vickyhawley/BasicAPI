241ce77b31df135f6a5d8718290dac1b58e3cc2c
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-03-04 23:44:14 +0000

    Update index.js

--- original/index.js
+++ original/index.js
@@ -24,24 +24,23 @@ root.innerHTML =`
 `;
 
 const input = document.querySelector('input');
-
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
-if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-}
-
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-
+    
+    if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+    }
+    
     resultsWrapper.innerHTML = '';
 
     dropdown.classList.add('is-active'); 
     for (let movie of movies) {
         const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : item.Poster;
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
         option.classList.add('dropdown-item');
         option.innerHTML = `
